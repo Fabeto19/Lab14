fbdc823b95fb886429257ed0341b0dc424405c62
Your Name <fabriciopolanco18@gmail.com>
2022-12-01 19:35:53 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import Index from './pages/Index';
+import NotFound from './pages/NotFound';
+import ForgotPassword from "./pages/ForgotPassword";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+
+} from "react-router-dom";
+
+function App() {
+  return (
+
+    <BrowserRouter>
+    <Routes>
+
+    <Route path="/" element={<Index/>}/>
+    <Route path ="/login" element={<Login/>}/>
+    <Route path="/forgotpassword" element={<ForgotPassword/>}/>
+    <Route path="/register" element={<Register/>}/>
+    <Route path="*" element={<NotFound/>} />
+    </Routes>
+    
+    
+    </BrowserRouter>
+   
+  );
+}
+
+export default App;
